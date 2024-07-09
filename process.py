8b6d70e614b0a118aba22cb33dd8aac5e0de53b8
ivan.li <ivan.li@thetradedesk.com>
2024-07-09 18:14:11 +0800

    parse response as json object

--- process.py
+++ process.py
@@ -1,3 +1,5 @@
+import json
+
 def chat(client, message):
     completion = client.chat.completions.create(
         # 👇 这里指定 Kimi 的模型名称
@@ -5,21 +7,30 @@ def chat(client, message):
 
         messages=[
             {"role": "user",
-             "content": message}
+             "content": message + ' According to the information above, construct a Business Canvas Model. '
+                                  'Return as json format for each of the keys: key_partners, key_activities, '
+                                  'key_resources, value_proposition, customer_relationship, channels, customer_segments'
+                                  ', cost_structure, revenue_streams.'}
         ],
+
+        response_format={"type": "json_object"},
+        n=1
+
     )
     return completion.choices[0].message.content
 
 def parse_answer(answer):
     # TODO: parse the response by keys of the BMC
-    return {
-        "key_partners": "this is some random answer",
-        "key_activities": "this is some random answer"*5,
-        "key_resources": "this is some random answer",
-        "value_proposition": "this is some random answer",
-        "customer_relationship": "this is some random answer",
-        "channels": "this is some random answer"*10,
-        "customer_segments": "this is some random answer",
-        "cost_structure": "this is some random answer",
-        "revenue_streams": "this is some random answer",
-            }
+    parsed_ans = json.loads(answer)
+    return parsed_ans
+    # return {
+    #     "key_partners": "this is some random answer",
+    #     "key_activities": "this is some random answer"*5,
+    #     "key_resources": "this is some random answer",
+    #     "value_proposition": "this is some random answer",
+    #     "customer_relationship": "this is some random answer",
+    #     "channels": "this is some random answer"*10,
+    #     "customer_segments": "this is some random answer",
+    #     "cost_structure": "this is some random answer",
+    #     "revenue_streams": "this is some random answer",
+    #         }
